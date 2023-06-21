8d95db5285ffffbe331cf40131b7e142048d9040
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-06-21 15:49:58 +0300

    Implement models in news/models.py and makemigrations

--- news/models.py
+++ news/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from newspaper_agency import settings
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=63)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="newspapers"
+    )
+
+    class Meta:
+        ordering = ["published_date"]
+
+    def __str__(self):
+        return f"{self.title}: {self.published_date}"
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self):
+        return self.username
