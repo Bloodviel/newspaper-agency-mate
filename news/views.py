9bd82690995929a981b4fdf07d1d7f1503dc6069
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-06-22 16:21:37 +0300

    Implement main page of app and add some information on it

--- news/views.py
+++ news/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from news.models import Redactor, Newspaper, Topic
+
+
+def index(request):
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper. objects.count()
+    num_topics = Topic.objects.count()
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topic": num_topics
+    }
+
+    return render(request, "news/index.html", context=context)
