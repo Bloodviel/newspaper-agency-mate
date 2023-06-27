024273fe86f19e7202d68c15a692abd3f9025753
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-06-27 19:14:51 +0300

    Add crispy-forms in settings.py and add crispy in templates

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_bootstrap4",
+    "crispy_forms",
     "news"
 ]
 
@@ -75,6 +77,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "newspaper_agency.wsgi.application"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
 # Database
