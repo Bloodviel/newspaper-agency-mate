d7b8df63185739da48fec8653d1fb18200d08dda
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-07-11 16:27:48 +0300

    Remove insecure Django secret key in setting and add build.sh

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure--x6)!!&_3%uc*t%u-4hmc!i-!!-cca_i!-&vh!t&1+7b0i&6=2")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
