bd584a699f54bbc41b595ae94eec11ec2e9ff8a2
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-07-11 14:11:48 +0300

    Update in views.py get_context_data method in all ListView classes

--- news/views.py
+++ news/views.py
@@ -33,7 +33,7 @@ class TopicListView(generic.ListView):
     paginate_by = 10
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(TopicListView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
         context["search_form"] = TopicSearchForm(
             initial={"name": name}
@@ -78,7 +78,7 @@ class NewspaperListView(generic.ListView):
     paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(NewspaperListView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         title = self.request.GET.get("title", "")
         context["search_form"] = NewspaperSearchForm(
             initial={"title": title}
@@ -124,7 +124,7 @@ class RedactorListView(generic.ListView):
     paginate_by = 10
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(RedactorListView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         username = self.request.GET.get("username", "")
         context["search_form"] = RedactorSearchForm(
             initial={"username": username}
